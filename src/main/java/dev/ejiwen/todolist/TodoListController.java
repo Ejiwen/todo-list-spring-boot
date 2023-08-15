9766c50e357f8ca4c12aad1dd48257c386dc7566
ejiwen <skibnewrim@gmail.com>
2023-08-15 16:33:12 -0700

    Created Note class.

--- src/main/java/dev/ejiwen/todolist/TodoListController.java
+++ src/main/java/dev/ejiwen/todolist/TodoListController.java
@@ -3,12 +3,14 @@ package dev.ejiwen.todolist;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ch.qos.logback.core.model.Model;
+
 @Controller
 public class TodoListController {
 
-    @GetMapping(value = "/hello")
-    public String handle() {
-        return "page1";
+    @GetMapping(value = "/notes")
+    public String getNotes(Model model) {
+        return "notes";
     }
 
 }
